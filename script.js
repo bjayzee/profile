f3dfaa8faf1187609094110b76511e47010a8ee5
bjayzee <bjayzee@gmail.com>
2024-07-30 08:25:30 +0100

    skills update

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Java Developer", "Fullstack Developer", "Technical Writer", "Frontend Developer", "Freelancer"],
+        strings: ["Backend Engineer", "Fullstack Developer", "Team Lead", "Instructor", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Entrepreneur", "Developer", "Technical Writer", "Serial Volunteer", "Freelancer"],
+        strings: ["Entrepreneur", "Software Engineer", "Serial Volunteer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
